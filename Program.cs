63304ec8c8ac839052324db2cfe2564804a46b35
e.serqueira <eduardo.serqueira@outlook.com>
2020-05-18 22:06:13 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio_seis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, diferenca;
+
+            Console.WriteLine("Entrada: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+
+            diferenca = (a * b - c * d);
+
+            Console.WriteLine("Diferença = {0}", diferenca);
+            Console.ReadKey();
+        }
+    }
+}
